6995a400f4bc24bd960daa23f8ba53f96a9430a7
mayeths <wow@huanghope.com>
2019-11-17 01:11:46 +0800

    Fix branch target in `ifu.sv`
    - Fix expression of branch_to with pc instead of pc4.
    - Rename some variables for more readable.

--- thinpad_top.srcs/sources_1/new/IFU/ifu.sv
+++ thinpad_top.srcs/sources_1/new/IFU/ifu.sv
@@ -19,18 +19,18 @@ module ifu(
   assign inst     = axi_data;
 
   //info
-  logic opcode        = inst[6:0];
-  logic is_jal        = opcode == `OP_JAL;
-  logic is_jalr       = opcode == `OP_JAL;
-  logic belong_branch = opcode == `OP_BRANCH;
-  logic imm32_neg     = inst[31] == 1;
-  logic do_branch     = belong_branch & imm32_neg;
+  wire [6:0] opcode  = inst[6:0];
+  wire is_jal        = opcode == `OP_JAL;
+  wire is_jalr       = opcode == `OP_JAL;
+  wire belong_branch = opcode == `OP_BRANCH;
+  wire imm32_neg     = inst[31] == 1;
+  wire do_branch     = belong_branch & imm32_neg;
   //bus
   assign jalr_addr = inst[11:7];
-  assign pc4       = pc + 4;
-  logic [31:0] imm32_Jtype = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};
-  logic [31:0] imm32_Btype = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
-  logic [31:0] branch_to   = imm32_Btype + pc4;
+  wire [31:0] immJal = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};
+  wire [31:0] immBxx = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
+  wire [31:0] pc4         = pc + 4;
+  wire [31:0] branch_to   = pc + immBxx;
 
   always @(posedge clk)begin
     //从特殊到一般
@@ -38,7 +38,7 @@ module ifu(
       rst       ? 32'b0 :
       stall     ? pc :
       bpfail    ? branch_backup :
-      is_jal    ? imm32_Jtype :
+      is_jal    ? immJal :
       is_jalr   ? jalr_data :
       do_branch ? branch_to :
                   pc4;
