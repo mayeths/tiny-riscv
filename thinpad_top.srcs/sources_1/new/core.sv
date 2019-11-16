8e02c733990cc5ed560cdeb267e2b7d48b1923d9
mayeths <wow@huanghope.com>
2019-11-16 21:36:57 +0800

    Add wires in `core.sv`
    - The outputs of all module have been created. Now what we need is just
      assign them to the next modules.

--- thinpad_top.srcs/sources_1/new/core.sv
+++ thinpad_top.srcs/sources_1/new/core.sv
@@ -1,5 +1,3 @@
-`include "defines.sv"
-
 module core (
 
   LFU_AXI
@@ -8,162 +6,202 @@ module core (
 
 );
 
-  LFU lfu_(
+  wire system_clk;
+  wire system_rst;
+
+  ////////output of modules////////
+  ////        ifu
+  wire [31:0] ifu_pc;
+  wire [31:0] ifu_inst;
+  wire        ifu_jalr_addr;
+  ////        pipeline1
+  wire [31:0] pipe1_pc;
+  wire [31:0] pipe1_inst;
+  ////        decode
+  wire        decode_rs1_enable;
+  wire        decode_rs2_enable;
+  wire [4:0]  decode_rs1_addr;
+  wire [4:0]  decode_rs2_addr;
+  wire [3:0]  decode_alu_action;
+  wire        decode_op1_is_pc;
+  wire        decode_op2_is_imm;
+  wire [31:0] decode_imm32;
+  wire        decode_dst_enable;
+  wire [4:0]  decode_dst_addr;
+  wire        decode_load_enable;
+  wire        decode_store_enable;
+  wire [2:0]  decode_load_type;
+  wire [1:0]  decode_store_type;
+  wire [11:0] decode_csr_addr;
+  wire        decode_csr_read_enable;
+  wire        decode_csr_write_enable;
+  wire [3:0]  decode_csru_action;
+  wire [31:0] decode_uimm32;
+  wire [2:0]  decode_exu_out_src;
+  ////        regfile
+  wire [31:0] regfile_rs1_data;
+  wire [31:0] regfile_rs2_data;
+  wire [31:0] regfile_jalr_data;
+  ////        csrfile
+  wire [31:0] csrfile_read_data;
+  ////        exu
+  wire [31:0] exu_exu_out;
+  wire [31:0] exu_csr_out;
+  ////        bpru
+  wire        bpru_bpfail;
+  ////        pipeline2
+  wire [31:0] pipe2_alu_out;
+  wire        pipe2_load_enable;
+  wire        pipe2_store_enable;
+  wire [2:0]  pipe2_load_type;
+  wire [1:0]  pipe2_store_type;
+  wire [31:0] pipe2_store_data;
+  wire        pipe2_dst_enable;
+  wire [4:0]  pipe2_dst_addr;
+  ////        lsu
+  wire        lsu_stall;
+  wire        lsu_load_data_valid;
+  wire [31:0] lsu_load_data;
+  ////        misc wires
+
+
+  IFU ifu_(
     //input
-    .clk(),
-    .rst(),
-    .stall(),   // 接 lsu 的 stall 信号，现在只有load/store会阻塞
-    .bpfail(),  // 接 exu 的 bpfail, branch predict failed.
+    .clk      (system_clk),
+    .rst      (system_rst),
+    .stall    (),
+    .bpfail   (),
+    .jalr_data(),
     //output
-    .pc(),
-    .inst(),
-    //jalr accelerate
-    .jalr_addr(),
-    .jalr_data()
+    .pc       (ifu_pc),
+    .inst     (ifu_inst),
+    .jalr_addr(ifu_jalr_addr)
   );
 
   pipeline1 pipeline1_(
-    .clk(),
-    .rst(),
-    .flush(),     // 接 exu 的 bpfail.
-    .stall(),     // 接 lsu 的 stall 信号
-    .if_pc(),     .ex_pc(),
-    .if_inst(),   .ex_inst()
+    .clk    (system_clk),
+    .rst    (system_rst),
+    .flush  (),
+    .stall  (),
+    .if_pc  (),              .ex_pc  (pipe1_pc),
+    .if_inst(),              .ex_inst(pipe1_inst)
   );
 
   decode decode_(
     //input
-    .inst(),              // pipeline1的ex_inst
+    .inst(),
     //output
-    .rs1_enable(),        // 接regfile的rs1_enable
-    .rs2_enable(),        // 接regfile的rs2_enable
-    .rs1_addr(),          // 接regfile的rs1_addr
-    .rs2_addr(),          // 接regfile的rs2_addr
-    .alu_action(),        // 接exu的alu_action指示alu进行的操作
-    .op1_is_pc(),         // 接exu指示op1使用pc还是rs1
-    .op2_is_imm(),        // 接exu指示op2使用imm32还是rs2
-    .imm32()              // 将instrution里的分散imm位整合成32bit形状再输出
-    .dst_enable(),        // 传入lsu阶段指示是否写回寄存器，由LSU阶段接回regfile的dst_enable。跳转和store置为0
-    .dst_addr(),          // 传入lsu阶段指示是否写回寄存器，由LSU阶段接回regfile的dst_enable。跳转和store置为0
-    .load_enable(),       // 传入lsu指示是否为load指令
-    .store_enable(),      // 传入lsu指示是否为store指令
-    .load_type(),         // 传入lsu指示load的类型
-    .store_type(),        // 传入lsu指示store的类型
-    .csr_addr(),          // 接csrfile的read_idx和write_idx
-    .csr_read_enable(),   // 接csrfile的read_enable
-    .csr_write_enable(),  // 接csrfile的write_enable
-    .csru_action(),       // 接exu的csru_action指示csru进行的操作
-    .uimm32(),            // 接exu的csru的uimm32
-    .exu_out_src()        // 接exu的exu_out_src指示exu应该输出什么
+    .rs1_enable      (decode_rs1_enable),
+    .rs2_enable      (decode_rs2_enable),
+    .rs1_addr        (decode_rs1_addr),
+    .rs2_addr        (decode_rs2_enable),
+    .alu_action      (decode_alu_action),
+    .op1_is_pc       (decode_op1_is_pc),
+    .op2_is_imm      (decode_op2_is_imm),
+    .imm32           (decode_imm32),
+    .dst_enable      (decode_dst_enable),
+    .dst_addr        (decode_dst_addr),
+    .load_enable     (decode_load_enable),
+    .store_enable    (decode_store_enable),
+    .load_type       (decode_load_type),
+    .store_type      (decode_store_type),
+    .csr_addr        (decode_csr_addr),
+    .csr_read_enable (decode_csr_read_enable),
+    .csr_write_enable(decode_csr_write_enable),
+    .csru_action     (decode_csru_action),
+    .uimm32          (decode_uimm32),
+    .exu_out_src     (decode_exu_out_src)
   );
 
   regfile regfile_(
     //input
-    .clk(),
-    .rst(),
-    .rs1_addr(),
-    .rs2_addr(),
-    .dst_addr(),
+    .clk       (system_clk),
+    .rst       (system_rst),
+    .rs1_addr  (),
+    .rs2_addr  (),
+    .dst_addr  (),
     .dst_enable(),
-    .dst_data(),
+    .dst_data  (),
+    .jalr_addr (),
     //output
-    .rs1_data(),
-    .rs2_data(),
-    //LFU jalr accelerate
-    .jalr_addr(),
-    .jalr_data()
+    .rs1_data (regfile_rs1_data),
+    .rs2_data (regfile_rs2_data),
+    .jalr_data(regfile_jalr_data)
   );
 
   csrfile csrfile_(
-    .clk(),
-    .rst(),
-    .read_enable(),
+    //input
+    .clk         (system_clk),
+    .rst         (system_rst),
+    .read_enable (),
     .write_enable(),
-    .read_idx(),
-    .write_idx(),
-    .write_data(),
-    .read_data()
+    .read_idx    (),
+    .write_idx   (),
+    .write_data  (),
+    //output
+    .read_data(csrfile_read_data)
   );
 
-  //NOTE: 目前的3级流水和regfile来说，不需要forward.
-  //  你要问为什么？看一下regfile的源码就好了，read部分特判了dst_addr进行dst_data bypass
-  //  当变成4+级流水的时候，forward非最后一级。最后一级不需要forward，减少工作量。
-  //Forward forward_(
-  //  //input
-  //  .rs1_addr(),
-  //  .rs2_addr(),
-  //  .dst_addr(),    // 检测rs1或rs2是否为上一指令的目的寄存器相同，是则forward
-  //  .rs1_data(),
-  //  .rs2_data(),
-  //  .dst_data(),
-  //  .dst_enable(),  // 上一指令是否写入寄存器，跳转和store置0从而不forward
-  //  //output
-  //  .rs1(),         // 接exu的rs1提供forward后的操作数1
-  //  .rs2(),         // 接exu的rs2提供forward后的操作数2
-  //);
-
-  //FIXME: auipc use pc.
   exu exu_(
     //input
-    .rs1(),
-    .rs2(),
-    .pc(),
-    .imm32(),
-    .op1_is_pc(),    // 指示op1使用pc还是rs1
-    .op2_is_imm(),   // 指示op2使用imm32还是rs2
-    .alu_action(),   // 指示alu进行的操作
-    .csr(),          // 接csrfile的read_data
+    .rs1        (),
+    .rs2        (),
+    .pc         (),
+    .imm32      (),
+    .op1_is_pc  (),
+    .op2_is_imm (),
+    .alu_action (),
+    .csr        (),
     .csru_action(),
-    .uimm32(),
+    .uimm32     (),
     .exu_out_src(),
     //output
-    .exu_out(),      // 传入pipeline2
-    .csr_out()
+    .exu_out(exu_exu_out),
+    .csr_out(exu_csr_out)
   );
 
   BPRU bpru_(
     //input
-    .is_beq(),
-    .is_bne(),
-    .is_blt(),
-    .is_bge(),
+    .is_beq (),
+    .is_bne (),
+    .is_blt (),
+    .is_bge (),
     .is_bltu(),
     .is_bgeu(),
     .alu_out(),
     //output
-    .bpfail()         // 传回lfu的bpfail表明branch predict是否错误
+    .bpfail(bpru_bpfail)
   );
 
   pipeline2 pipepine2_(
-    .clk(),
-    .rst(),
+    .clk  (system_clk),
+    .rst  (system_rst),
     .flush(),
     .stall(),
-    .exu_alu_out(),        .lsu_alu_out(),
-    .exu_load_enable(),    .lsu_load_enable(),
-    .exu_load_type(),      .lsu_load_type(),
-    .exu_store_enable(),   .lsu_store_enable(),
-    .exu_store_type(),     .lsu_store_type(),
-    .exu_store_data(),     .lsu_store_data(),
-    .exu_dst_enable(),     .lsu_dst_enable(),
-    .exu_dst_addr(),       .lsu_dst_addr()
+    .exu_alu_out     (),            .lsu_alu_out     (pipe2_alu_out),
+    .exu_load_enable (),            .lsu_load_enable (pipe2_load_enable),
+    .exu_load_type   (),            .lsu_load_type   (pipe2_load_type),
+    .exu_store_enable(),            .lsu_store_enable(pipe2_store_enable),
+    .exu_store_type  (),            .lsu_store_type  (pipe2_store_type),
+    .exu_store_data  (),            .lsu_store_data  (pipe2_store_data),
+    .exu_dst_enable  (),            .lsu_dst_enable  (pipe2_dst_enable),
+    .exu_dst_addr    (),            .lsu_dst_addr    (pipe2_dst_addr)
   );
 
   LSU lsu_(
-    // TODO: AXI握手完成load/store
     //input
-    .clk(),
-    .rst(),
-    .load_enable(),
-    .load_type(),       // lb, lh, lw, lbu, lhu?
+    .clk         (system_clk),
+    .rst         (system_rst),
+    .load_enable (),
+    .load_type   (),
     .store_enable(),
-    .store_type(),      // sb, sh, sw?
-    .addr(),            // 接lsu_alu_out
-    .store_data(),
+    .store_type  (),
+    .addr        (),
+    .store_data  (),
     //output
-    .stall(),           // 若load/store有效，则 stall ifu和两个流水线，直到l/s完成
-    .load_data_valid(),
-    .load_data()
+    .stall          (lsu_stall),
+    .load_data_valid(lsu_load_data_valid),
+    .load_data      (lsu_load_data)
   );
 
   // TODO: write back to regfile
